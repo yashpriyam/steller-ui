55a64bfee7dbb04bdddf724423a759cb3c0da41c
pinkeshroy <pinkeshyadav9661@gmail.com>
2023-11-28 15:58:40 +0530

    onHover_done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,23 @@
+import { useEffect, useState } from "react";
 import { Button } from "./components/button/button";
 // interface AppProps {}
-const App:React.FC = () => {
+const App: React.FC = () => {
+  const [className, setClassName] = useState<string>('buttonComp')
   // const { t } = useTranslation();
     const onClick = () => {
       // setIsModalOpen(true);
     };
 
-    const onHover = () => {
-      // setIsModalOpen(false);
+  const onHover = () => {
+      setClassName('hoverOnButton')
     };
-
+  useEffect(() => {
+  setClassName("buttonComp");
+ },[])
     return (
       <div>
         <h1>React Button Example</h1>
-        <Button className="buttonComp" onClick={onClick} onHover={onHover} isDisabled={false} size={"large"} isHidden={false} variant={"outlined"} />
+        <Button className={className} onClick={onClick} onHover={onHover} isDisabled={false} size={"large"} isHidden={false} variant={"outlined"} />
       </div>
     );
 }
