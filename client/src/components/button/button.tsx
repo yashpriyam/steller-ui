e9674d1372cb6504e3a4741004e1a21605074bee
pinkeshroy <pinkeshyadav9661@gmail.com>
2023-11-29 20:13:21 +0530

    WMB1-61 cursor on disabled updated

--- client/src/components/button/button.tsx
+++ client/src/components/button/button.tsx
@@ -1,91 +1,78 @@
 import React from "react";
-import './button.scss';
+import "./button.scss";
 import { Loader } from "../loader/loader";
 interface ButtonProps {
-  className?: string;
-  isDisabled?: boolean;
-  onClick ?: () => void; 
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   onHover?: () => void;
   size?: "small" | "medium" | "large";
   variant?: "text" | "contained" | "outlined";
-  isHidden: boolean;
-  iconOnLeft?: string;
-  iconOnRight?: string;
-  iconOnCentre?: string;
+  iconPosition?: "left" | "center" | "right";
+  className?: string;
+  isDisabled?: boolean;
+  isHidden?: boolean;
+  icon?: string;
   isLoading?: boolean;
-  loaderPosition?: "left" | "center" | "right";
   loaderIcon?: string;
+  text?: string;
 }
 
 export const Button: React.FC<ButtonProps> = ({
-  className,
-  isDisabled = false,
-  onClick = () => {},
+  onClick = (e:React.MouseEvent<HTMLButtonElement>) => {},
   onHover = () => {},
   size = "medium",
   variant = "contained",
-  isHidden = false,
-  iconOnLeft,
-  iconOnRight,
-  iconOnCentre,
-  isLoading = false,
-  loaderPosition = "center",
+  iconPosition = "left",
+  text = "submit",
+  className,
+  isDisabled,
+  isHidden,
+  icon,
+  isLoading,
   loaderIcon,
 }: ButtonProps) => {
-  const variantClassName: Record<string, string> = {
+  const variantMap: Record<string, string> = {
     text: "text-button-style",
     outlined: "outlined-button-style",
     contained: "contained-button-style",
   };
-  const sizeClassName: Record<string, string> = {
+  const sizeMap: Record<string, string> = {
     small: "small-fontsize",
     medium: "medium-fontsize",
     large: "large-fontsize",
   };
-  const loaderPositionClassName: Record<string, string> = {
-    left: "loader-position-left",
-    center: "loader-position-center",
-    right: "loader-position-right",
+  const iconPositionMap: Record<string, string> = {
+    left: "icon-position-left",
+    center: "icon-position-center",
+    right: "icon-position-right",
   };
-
   return (
     <>
       {!isHidden && (
         <button
-          className={`button-component ${className} ${variantClassName[variant]} ${sizeClassName[size]}`}
+          className={`button-component ${className} ${variantMap[variant]} ${sizeMap[size]}`}
           disabled={isDisabled}
           onClick={onClick}
           onMouseEnter={onHover}
         >
-          {(isLoading && (
+          {isLoading || icon ? (
             <span
-              className={`loader-postion-style ${loaderPositionClassName[loaderPosition]}`}
+              className={`loader-postion-style ${iconPositionMap[iconPosition]}`}
             >
-              <span className="left-submit button-text">Submit</span>
-              {loaderIcon ? (
+              <span className="loader-left-button loader-button">{text}</span>
+              {(isLoading ? loaderIcon : icon) ? (
                 <img
                   className="loader-icon"
-                  src={loaderIcon}
-                  alt="loading..."
+                  src={isLoading ? loaderIcon : icon}
+                  alt=""
                 />
               ) : (
                 <Loader />
               )}
-              <span className="right-submit button-text">Submit</span>
+              <span className="loader-right-button loader-button">{text}</span>
             </span>
-          )) ||
-            (iconOnCentre && <img src={iconOnCentre} alt="loading..." />) ||
-            (iconOnLeft && (
-              <span>
-                <img src={iconOnLeft} alt="loading..." /> Submit
-              </span>
-            )) ||
-            (iconOnRight && (
-              <span>
-                Submit
-                <img src={iconOnLeft} alt="loading..." />
-              </span>
-            )) || <span>Submit</span>}
+          ) : (
+            <span>{text}</span>
+          )}
         </button>
       )}
     </>
