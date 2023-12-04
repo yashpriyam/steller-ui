297aa4f9e4fe034efdfb7717f2e800ac26536b49
Obliger0 <sujalgotharwal904@gmail.com>
2023-12-04 12:20:37 +0530

    WMB1-77: Checkbox comp and style implemented, color vars are yet to be implement

--- client/src/components/checkbox/Checkbox.tsx
+++ client/src/components/checkbox/Checkbox.tsx
@@ -1,55 +1,61 @@
 import React, { useState } from "react";
-
+import "./checkbox.scss";
+import checkIcon from "../../icons/check-icon.svg";
 interface CheckboxProps {
   className?: string;
   options?: string[];
+  checkboxColor?: string;
   onSelect?: (currentSelected: string, selectedValues: string[]) => void;
 }
 
 export const Checkbox: React.FC<CheckboxProps> = ({
-  className = "",
-  options=[],
-  onSelect=()=>{},
+  className,
+  options = [],
+  checkboxColor,
+  onSelect = () => {},
 }) => {
-  const [selectedIndexList, setSelectedIndexList] = useState<number[]>([]);
+  const [selectedValues, setSelectedValues] = useState<Record<number, boolean>>(
+    {}
+  );
 
-  const handleCheckboxChange = (
-    e: any,
-    index: number
-  ) => {
-    console.log(e.target.checked);
-    
-    const isSelected = selectedIndexList.includes(index);
-    console.log({isSelected});
-    
-    let updatedIndexes: number[];
-
-    if (isSelected) {
-      updatedIndexes = selectedIndexList.filter((i) => i !== index);
+  const handleCheckboxChange = (index: number) => {
+    const newSelectedValues = { ...selectedValues };
+    if (newSelectedValues[index]) {
+      delete newSelectedValues[index];
     } else {
-      updatedIndexes = [...selectedIndexList, index];
+      newSelectedValues[index] = true;
     }
-
-    setSelectedIndexList(updatedIndexes);
-
-    const selectedValues = updatedIndexes.map((i) => options[i]);
-    onSelect(options[index], selectedValues);
+    setSelectedValues(newSelectedValues);
+    onSelect(options[index], Object.keys(newSelectedValues));
   };
 
   return (
     <div className={`checkbox-container ${className}`}>
       {options.map((option, index) => (
-        <label key={index} className="checkbox-label">
-          <input
-            type="checkbox"
-            // checked={selectedIndexList.includes(index)}
-            onChange={(e) => handleCheckboxChange(e,index)}
-          />
-          {option}
-        </label>
+        <div
+          className="checkbox-label"
+          onClick={() => handleCheckboxChange(index)}
+        >
+          <div
+            style={{
+              backgroundColor: checkboxColor,
+              borderColor: checkboxColor,
+            }}
+            className="custom-checkbox"
+          >
+            {selectedValues[index] && (
+              <img className="checkmark-icon" src={checkIcon} alt="icon" />
+            )}
+          </div>
+          <div
+            className="checkbox-wrapper"
+            style={{
+              backgroundColor: checkboxColor,
+            }}
+          ></div>
+          <label className="label-text">{option}</label>
+        </div>
       ))}
     </div>
   );
 };
-
-export default Checkbox;
