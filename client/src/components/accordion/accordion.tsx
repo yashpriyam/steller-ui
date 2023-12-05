26c61c610a76051a7d644e6c3c9ad7f91b8effed
Obliger0 <sujalgotharwal904@gmail.com>
2023-12-05 20:00:41 +0530

    WMB1-82: added filter checkbox & styled accordion

--- client/src/components/accordion/accordion.tsx
+++ client/src/components/accordion/accordion.tsx
@@ -10,16 +10,28 @@ interface AccordionProps {
 const Accordion: React.FC<AccordionProps> = ({ title, children }: AccordionProps) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div onClick={(e) => { setIsOpen(!isOpen); e.stopPropagation() }} className='accordion-container'>
-      <div className='accordion-title'>
+    <div className="accordion-container">
+      <div
+        className="accordion-title"
+        onClick={(e) => {
+          setIsOpen(!isOpen);
+          e.stopPropagation();
+        }}
+      >
         {title}
-        <img className={`accordion-dropdown-arrow ${isOpen && 'accordion-dropdown-arrow-active'}`} src={dropdownArrow} alt="" />
+        <img
+          className={`accordion-dropdown-arrow ${
+            isOpen && "accordion-dropdown-arrow-active"
+          }`}
+          src={dropdownArrow}
+          alt=""
+        />
       </div>
-      <div className={`accordion-children ${isOpen && 'open-accordion'}`}>
+      <div className={`accordion-children ${isOpen && "open-accordion"}`}>
         {children}
       </div>
     </div>
-  )
+  );
 }
 
 export default Accordion
---
+++ client/src/pages/scheduling/accordionDataList.js
@@ -0,0 +1,214 @@
+export const accordionDataList = [
+  {
+    title: "Week 1",
+    description:
+      "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
+    days: [
+      {
+        title: "day 1",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 2",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 3",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 4",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 5",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+    ],
+  },
+  {
+    title: "Week 2",
+    description:
+      "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
+    days: [
+      {
+        title: "day 1",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 2",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 3",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 4",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 5",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+    ],
+  },
+  {
+    title: "Week 3",
+    description:
+      "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
+    days: [
+      {
+        title: "day 1",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 2",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 3",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 4",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 5",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+    ],
+  },
+  {
+    title: "Week 4",
+    description:
+      "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
+    days: [
+      {
+        title: "day 1",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 2",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 3",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 4",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 5",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+    ],
+  },
+  {
+    title: "Week 5",
+    description:
+      "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
+    days: [
+      {
+        title: "day 1",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 2",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 3",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 4",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+      {
+        title: "day 5",
+        description: "",
+        videoUrl: "",
+        notesUrl: "",
+        assignmentUrl: "",
+      },
+    ],
+  },
+];
+
+export const checkboxDataList = ["Topic", "Week","HTML","CSS","JavaScript"];
