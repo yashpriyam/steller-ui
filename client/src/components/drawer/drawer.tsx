6c7ee8f44af0764b255b25e85c3bce65fd3a6456
pinkeshroy <pinkeshyadav9661@gmail.com>
2023-12-05 15:16:09 +0530

    WMB1-79-create-drawer-component-in-webmaster-portal

--- client/src/components/drawer/drawer.tsx
+++ client/src/components/drawer/drawer.tsx
@@ -1,14 +1,15 @@
 import "./drawer.scss";
-import { useState } from 'react'
-import { GiHamburgerMenu } from "react-icons/gi";
+import { useState, useRef } from "react";
+import useOnOutsideClick from "../../hooks/useOnOutsideClick";
 
 interface DrawerProps {
+  direction?: "left" | "right" | "top" | "bottom";
+  isOpen?: true | false;
   className?: string;
-  direction?: "left"|"right"|"top"|"bottom";
-  isOpen?: true|false;
+  icon?: string;
   style?: object;
   options?: {
-    onClick: (e:React.MouseEvent<HTMLDivElement>) => void;
+    onClick: (e: React.MouseEvent<HTMLDivElement>) => void;
     text: string;
     url: string;
   }[];
@@ -16,39 +17,46 @@ interface DrawerProps {
 
 export const Drawer: React.FunctionComponent<DrawerProps> = ({
   direction = "right",
+  isOpen = false,
   options,
   className,
-  isOpen = false,
   style,
+  icon
 }: DrawerProps) => {
   const drawerContainerMap: Record<string, string> = {
     left: "drawer-left-open",
     right: "drawer-right-open",
     top: "drawer-top-open",
     bottom: "drawer-bottom-open",
-    };
-    const [isOpenDrawer, setIsOpenDrawer] = useState<boolean>(false);
-    const onClickFun = () => {
-        setIsOpenDrawer(!isOpenDrawer)
-    }
+  };
+  const [isOpenDrawer, setIsOpenDrawer] = useState<boolean>(isOpen);
+  const drawerRef = useRef<HTMLDivElement | null>(null);
+  useOnOutsideClick(drawerRef, () => setIsOpenDrawer(false));
   return (
-      <div className={`drawer-wrapper-container ${className}`}>
-          <div className="drawer-toggle-button" onClick={onClickFun}><GiHamburgerMenu/></div>
-      {isOpenDrawer&&<div
-        className={`drawer-container ${drawerContainerMap[direction]} ${className}`}
-      >
-        {options?.map((option, index) => {
-          return (
-            <div className="drawer-option" onClick={option.onClick}>
-              <span className="drawer-option-icon">
-                <img src={option.url} alt="" />
-              </span>
-              <span className="drawer-option-text">{option.text}</span>
-            </div>
-          );
-        })}
-      </div>}
-    </div>
+    <>
+      {isOpenDrawer && (
+        <div
+          ref={drawerRef} style={style}
+          className={`drawer-container ${drawerContainerMap[direction]} ${className}`}
+        >
+          <div  className="web-master-icon-container">
+            <img className="web-master-icon"
+              src={icon}
+              alt=""
+            />
+          </div>
+          {options?.map((option, index) => {
+            return (
+              <div className="drawer-option" onClick={option.onClick}>
+                <span className="drawer-option-icon">
+                  {option.url && <img src={option.url} alt="" />}
+                </span>
+                <span className="drawer-option-text">{option.text}</span>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
-
