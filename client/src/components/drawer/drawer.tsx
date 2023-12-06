359458d63535f39cbc98f5895b2eb971cd8c201e
pinkeshroy <pinkeshyadav9661@gmail.com>
2023-12-06 19:18:59 +0530

    WMB1-79 update pr comments and fix oever flow on right direction

--- client/src/components/drawer/drawer.tsx
+++ client/src/components/drawer/drawer.tsx
@@ -21,7 +21,7 @@ export const Drawer: React.FunctionComponent<DrawerProps> = ({
   options,
   className,
   style,
-  icon
+  icon,
 }: DrawerProps) => {
   const drawerContainerMap: Record<string, string> = {
     left: "drawer-left-open",
@@ -29,34 +29,46 @@ export const Drawer: React.FunctionComponent<DrawerProps> = ({
     top: "drawer-top-open",
     bottom: "drawer-bottom-open",
   };
+  const drawerCloseMap: Record<string, string> = {
+    left: "drawer-left-close",
+    right: "drawer-right-close",
+    top: "drawer-top-close",
+    bottom: "drawer-bottom-close",
+  };
   const [isOpenDrawer, setIsOpenDrawer] = useState<boolean>(isOpen);
   const drawerRef = useRef<HTMLDivElement | null>(null);
   useOnOutsideClick(drawerRef, () => setIsOpenDrawer(false));
   return (
     <>
-      {isOpenDrawer && (
+      {
         <div
-          ref={drawerRef} style={style}
-          className={`drawer-container ${drawerContainerMap[direction]} ${className}`}
+          ref={drawerRef}
+          style={style}
+          className={`drawer-container ${drawerContainerMap[direction]} ${
+            !isOpenDrawer && drawerCloseMap[direction]
+          } ${className}`}
         >
-          <div  className="web-master-icon-container">
-            <img className="web-master-icon"
-              src={icon}
-              alt=""
-            />
+          <div className="drawer-icon-container">
+            <img className="drawer-icon" src={icon} alt="" />
           </div>
           {options?.map((option, index) => {
             return (
               <div className="drawer-option" onClick={option.onClick}>
                 <span className="drawer-option-icon">
-                  {option.url && <img src={option.url} alt="" />}
+                  {option.url && (
+                    <img
+                      className="drawer-icon-image"
+                      src={option.url}
+                      alt=""
+                    />
+                  )}
                 </span>
                 <span className="drawer-option-text">{option.text}</span>
               </div>
             );
           })}
         </div>
-      )}
+      }
     </>
   );
 };
