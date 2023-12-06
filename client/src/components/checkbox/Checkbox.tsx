c72f6256eb4120d7af1229475a0824b23e23face
Obliger0 <sujalgotharwal904@gmail.com>
2023-12-06 18:58:27 +0530

    WMB1-80: title added, fixed review comments

--- client/src/components/checkbox/Checkbox.tsx
+++ client/src/components/checkbox/Checkbox.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./checkbox.scss";
 import checkIcon from "../../icons/check-icon.svg";
 interface CheckboxProps {
   className?: string;
-  options: string[];
+  options: {text: string, value : string}[];
   bgColor?: string;
   textColor?: string;
   onSelect?: (currentSelected: string, selectedValues: string[]) => void;
   style?: {};
-  checkboxDirection?: "row" | "column";
+  direction?: "row" | "column";
+  title?: string;
 }
 
 export const Checkbox: React.FC<CheckboxProps> = ({
@@ -18,55 +19,86 @@ export const Checkbox: React.FC<CheckboxProps> = ({
   bgColor,
   style,
   textColor,
-  checkboxDirection="column",
+  direction="column",
+  title,
+
 }) => {
   const [selectedValues, setSelectedValues] = useState<Record<number, boolean>>(
     {}
   );
+  const [hoverBg, setHoverBg] = useState<boolean[]>([]);
 
+  useEffect(()=>{
+    const newHoverBg : boolean[] = options.map(elem=>false);
+    setHoverBg(newHoverBg);
+  },[options])
+  
   const handleCheckboxChange = (index: number) => {
-    const newSelectedValues = { ...selectedValues };
+    const newSelectedValues : Record<number,boolean> = { ...selectedValues };
     if (newSelectedValues[index]) {
       delete newSelectedValues[index];
     } else {
       newSelectedValues[index] = true;
     }
     setSelectedValues(newSelectedValues);
-    onSelect(options[index], Object.keys(newSelectedValues));
+    onSelect(options[index].value, Object.keys(newSelectedValues));
   };
 
   return (
-    <div className={`checkbox-container ${className}`} style={{flexDirection:checkboxDirection}}>
-      {options.map((option, index) => (
-        <div
-          className="checkbox-label"
-          onClick={() => handleCheckboxChange(index)}
-        >
+    <div className={`checkbox-container ${className}`}>
+      {title && <p className="title">{title}</p>}
+      <div
+        className="checkbox-label-wrapper"
+        style={{ flexDirection: direction }}
+      >
+        {options.map((option, index) => (
           <div
-            style={{
-              ...style,
-              backgroundColor: bgColor,
-              borderColor: bgColor,
-            }}
-            className="custom-checkbox"
-          >
-            {selectedValues[index] && (
-              <img className="checkmark-icon" src={checkIcon} alt="icon" />
-            )}
-          </div>
-          <div
-            className="checkbox-wrapper"
-            style={{
-              ...style,
-              backgroundColor: bgColor,
-            }}
+            className="checkbox-label"
             onClick={() => handleCheckboxChange(index)}
-          ></div>
-          <label className="label-text" style={{ color: textColor }}>
-            {option}
-          </label>
-        </div>
-      ))}
+          >
+            <div
+              className="checkbox-wrapper"
+              style={{ borderColor: bgColor }}
+              onMouseOver={() => {
+                hoverBg[index] = true;
+                setHoverBg([...hoverBg]);
+              }}
+              onMouseOut={() => {
+                hoverBg[index] = false;
+                setHoverBg([...hoverBg]);
+              }}
+            >
+              {hoverBg[index] && (
+                <div
+                  className={`${
+                    selectedValues[index] && "animated-checkbox-bg"
+                  } checkbox-bg`}
+                  style={{
+                    ...style,
+                    backgroundColor: `${hoverBg[index] && bgColor}`,
+                    opacity: 0.2,
+                  }}
+                ></div>
+              )}
+              {selectedValues[index] && (
+                <div
+                  style={{
+                    ...style,
+                    backgroundColor: bgColor,
+                    borderColor: bgColor,
+                  }}
+                  className="custom-checkbox"
+                >
+                  <img className="checkmark-icon" src={checkIcon} alt="icon" />
+                </div>
+              )}
+            </div>
+            <label className="label-text" style={{ color: textColor }}>
+              {option?.text}
+            </label>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
