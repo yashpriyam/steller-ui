41892f22ef6945306b3dda905892783e0d639b3a
kratiks26 <kratiks26@gmail.com>
2023-12-08 17:55:59 +0530

    WMB1-87: change request resolve

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,25 @@
 import { useTranslation } from "react-i18next";
+import { Modal } from './components/modal/modal'
+import React,{useState} from "react";
 
 
 const App = () => {
   const { t } = useTranslation();
+  const[isOpen, setIsOpen] = useState<boolean>(false);
 
+    const handleModalOpen = () =>{
+        setIsOpen(true);
+    }
   return (
     <div>
       <h1>{t("greeting")}</h1>
+      <button onClick={handleModalOpen}> to active modal</button>
+
+      <Modal isClosable ={true} onClose = { ()=>{setIsOpen(false)}} isOpen = {isOpen}>
+        <div>
+          operating from outside the model component
+        </div>
+      </Modal>
     </div>
   );
 };
