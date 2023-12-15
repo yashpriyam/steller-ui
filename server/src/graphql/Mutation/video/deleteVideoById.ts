62cfc244b9e9f70c0993904ba05ec38a1c325bba
Obliger0 <sujalgotharwal904@gmail.com>
2023-12-15 17:00:31 +0530

    PI-11: conflits resolved

--- server/src/graphql/Mutation/video/deleteVideoById.ts
+++ server/src/graphql/Mutation/video/deleteVideoById.ts
@@ -7,25 +7,26 @@ export const deleteVideoById = async (
   args: { videoId: ObjectId }
 ): Promise<VideoOutputDataType | void> => {
   const { VIDEO_DELETION_FAILED } = errorMessages.VIDEO_MODEL;
+  const errorData: CustomResponseType = {
+    status: statusCodes.BAD_REQUEST,
+    message: VIDEO_DELETION_FAILED,
+  };
   try {
     const { VIDEO_DELETION_SUCCESS } = localMessages.VIDEO_MODEL;
     const { videoId } = args;
     const deletedVideoData = await videoModel.findByIdAndDelete(videoId);
     return {
       videoData: deletedVideoData,
-      response: {
-        status: statusCodes.OK,
-        message: deletedVideoData
-          ? VIDEO_DELETION_SUCCESS
-          : VIDEO_DELETION_FAILED,
-      },
+      response: deletedVideoData
+        ? {
+          status: statusCodes.OK,
+          message: VIDEO_DELETION_SUCCESS,
+        }
+        : errorData,
     };
   } catch (err) {
     return {
-      response: {
-        status: statusCodes.BAD_REQUEST,
-        message: VIDEO_DELETION_FAILED,
-      },
+      response: errorData,
     };
   }
 };
