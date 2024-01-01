993c888c58f1d099cb3187a00206ec207348c069
Arzoo-25 <ajarzoo25@gmail.com>
2024-01-01 23:00:13 +0530

    Done changes, giving error if MonorepoIndex is comment out.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,11 +6,8 @@ import MonorepoIndex from "./monorepoClient/MonorepoIndex";
 
 
 const App = () => {
-  // const { t } = useTranslation();
   return (
     <div>
-      {/* <h1>{t("greeting")}</h1> */}
-      {/* <DatePicker label="Date Picker" labelPosition="left" isRequired={true} maxDate="2023-12-12" minDate="2011-01-01" icon="📆"/> */}
       <LoginUI/>
        {/* <MonorepoIndex /> */}
     </div>
