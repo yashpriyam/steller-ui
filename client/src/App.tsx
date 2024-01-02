137bd23faf88a46696e76b8d3103aa0a369cda3a
dhananjayadav <dhananjay2451@gmail.com>
2024-01-02 08:24:00 +0530

    PI-110: remove calling api code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import MonorepoIndex from "./monorepoClient/MonorepoIndex";
-import { useUser } from "./redux/actions/userAction";
 
 
 const App = () => {
-  const { users, registerUser } = useUser();
-  useEffect(() => {
-    registerUser({
-      name: 'dadad',
-      email: 'abcd@gmail.coma',
-      phoneNumber: '1234567890',
-      occupation: 'SDE-1',
-      sessionPreference: 'online',
-      expectedSalary: '2CR',
-      isJobSeeker: false,
-    })
-  }, [])
   return (
     <div>
       <MonorepoIndex />
