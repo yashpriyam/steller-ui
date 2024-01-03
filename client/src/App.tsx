f360c5bd45263eec6182e639a4ce11536d771943
Obliger0 <sujalgotharwal904@gmail.com>
2024-01-03 16:43:21 +0530

    WMB1-82: refactored code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import React  from "react";
 import MonorepoIndex from "./monorepoClient/MonorepoIndex";
-import Scheduling from "./pages/scheduling/scheduling";
 
 
 const App = () => {
   return (
     <div>
-       <Scheduling />
+       <MonorepoIndex />
     </div>
   );
 };
