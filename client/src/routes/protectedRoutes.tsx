96bcc8d5953e64ff3112d25c3a8a6b6d4ba4bf1c
dhananjayadav <dhananjay2451@gmail.com>
2024-01-04 07:48:23 +0530

    PI-134: add logic for protected and unprotected routes

---
+++ client/src/routes/protectedRoutes.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const protectedRoutesMap: RoutesMapInterface = Object.freeze({
+    // Add your protected routes here
+});
+
+export const getProtectedRoutes = (): React.ReactNode[] => {
+    /* TODO:@dhananjay - Need to add login check for protected routes  */
+    return Object.entries(protectedRoutesMap).map(([path, element]) => (
+        <Route key={path} path={path} element={element} />
+    ));
+};
