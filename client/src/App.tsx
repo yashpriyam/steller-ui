15b9b69ef64aa7dc05c15f56a424ceb6f2c8a999
pinkeshroy <pinkeshyadav9661@gmail.com>
2024-01-04 13:34:57 +0530

    created input component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import MonorepoIndex from "./monorepoClient/MonorepoIndex";
+import { InputComponent } from "./components/input/inputComponent";
 
 
 const App = () => {
+  const [value, setValue] = useState("");
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  };
   return (
     <div>
-      <MonorepoIndex />
+      <InputComponent placeholder="password" type="password" value={value} onChange={onChange}/>
     </div>
   );
 };
