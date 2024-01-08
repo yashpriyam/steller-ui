39ad7425a584a24288cbf6e16426c4ca12073209
pinkeshroy <pinkeshyadav9661@gmail.com>
2024-01-08 18:31:14 +0530

    ceated api for login paid user

--- server/src/graphql/Mutation/auth/login.ts
+++ server/src/graphql/Mutation/auth/login.ts
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken"; 
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { paidUser } from "@models";
 import { errorMessages, localMessages, statusCodes } from "@constants";
@@ -7,32 +7,42 @@ export const login = async (
   _parent: undefined,
   args: { data: loginUserInputType },
   { res }: ContextType
-): Promise<unknown> => {
+): Promise<CustomResponseType | unknown> => {
+  const { PAID_USER_LOGIN_SUCCESS } = localMessages.PAID_USER_MODEL;
+  const { PAID_USER_LOGIN_FAILED } = errorMessages.PAID_USER_MODEL;
+  const errorData: CustomResponseType = {
+    message: PAID_USER_LOGIN_FAILED,
+    status: statusCodes.BAD_REQUEST,
+  };
   try {
     const { email, password } = args.data;
     const user = await paidUser.findOne({ email });
-     const hashPassword = bcrypt.hashSync(password, user?.password?.salt||"");
-      const isValidPassword = user?.password?.hash === hashPassword;
-     if (!user || !isValidPassword) {
-       return {
-         response: {
-           message: "Invalid email or password",
-           status: statusCodes.BAD_REQUEST,
-         },
-       };
-     }
+    const hashPassword = bcrypt.hashSync(password, user?.password?.salt || "");
+    const isValidPassword = user?.password?.hash === hashPassword;
+    if (!user || !isValidPassword) {
+      return {
+        response: errorData,
+      };
+    }
+    const jwtSecret = process.env.JWT_SECRET;
+    const jwtToken = process.env.JWT_TOKEN;
+    if (jwtSecret && jwtToken) {
+      const token = jwt.sign(user, jwtSecret, {
+        expiresIn: "1h",
+      });
+      res.cookie(jwtToken, token, {
+        httpOnly: true,
+      });
+    }
     return {
       response: {
-        message: "login successfully",
+        message: PAID_USER_LOGIN_SUCCESS,
         status: statusCodes.OK,
       },
     };
   } catch (error) {
     return {
-      response: {
-        message: "login failed",
-        status: 400,
-      },
+      response: errorData,
     };
   }
 };
