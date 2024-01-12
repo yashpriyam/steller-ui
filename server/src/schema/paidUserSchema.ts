62df9c6fa58ad423e159311a8ecadae73618b917
isagarjaiwal <sagarjaiswal815555@gmail.com>
2024-01-13 00:04:52 +0530

    Update paid user Schema

--- server/src/schema/paidUserSchema.ts
+++ server/src/schema/paidUserSchema.ts
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 
-const sessionPreferenceEnum = { online: "online", offline: "offline" };
-const paidUserSchema = new mongoose.Schema({
+enum SessionPreferenceEnum {
+  online = "online",
+  offline = "offline",
+}
+
+const paidUserSchema = new mongoose.Schema<paidUserSchemaType>({
   username: {
     type: String,
     required: true,
@@ -30,7 +34,7 @@ const paidUserSchema = new mongoose.Schema({
   },
   sessionPreference: {
     type: String,
-    enum: sessionPreferenceEnum,
+    enum: SessionPreferenceEnum,
     required: false,
   },
   professionalStatus: {
@@ -68,10 +72,62 @@ const paidUserSchema = new mongoose.Schema({
     required: false,
   },
   password: {
-    type:String
-  }
+    type: String
+  },
+  personalDetail: {
+    fullName: { type: String, required: true },
+    headline: { type: String },
+  },
+  socialDetail: {
+    address: {
+      colony: { type: String },
+      city: { type: String },
+    },
+    phoneNumber: { type: String },
+    gmail: { type: String },
+    githubLink: { type: String },
+    linkedInLink: { type: String },
+  },
+  experienceData: [{
+    companyName: { type: String },
+    companyLocation: { type: String },
+    role: { type: String },
+    startDate: { type: String },
+    endDate: { type: String },
+    description: { type: [String] },
+    techStack: { type: [String] },
+  }],
+  projectsData: [{
+    heading: { type: String },
+    description: { type: [String] },
+    deployLink: { type: String },
+    gitHubLink: { type: String },
+    techStack: { type: [String] },
+  }],
+  skillsData: {
+    language: { type: [String] },
+    frontend: { type: [String] },
+    backend: { type: [String] },
+    database: { type: [String] },
+    versionControl: { type: [String] },
+    cIcD: { type: [String] },
+  },
+  educationalData: [{
+    instituteName: { type: String },
+    location: { type: String },
+    course: { type: String },
+    startDate: { type: String },
+    endDate: { type: String },
+    cgpa: { type: String },
+  }],
+  achievementsData: [{
+    icon: { type: String },
+    header: { type: String },
+    description: { type: String },
+    links: { type: String },
+  }],
 });
 
-const paidUser = mongoose.model("PaidUser", paidUserSchema);
+const paidUser = mongoose.model<paidUserSchemaType>("PaidUser", paidUserSchema);
 
 export { paidUser };
