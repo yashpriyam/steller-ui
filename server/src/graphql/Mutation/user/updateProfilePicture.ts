19e0dfbd91d1436b822edbf42250667d894ce6d2
kratiks26 <kratiks26@gmail.com>
2024-01-16 10:42:09 +0530

    PI-152-update-upload-profile-picture-api

--- server/src/graphql/Mutation/user/updateProfilePicture.ts
+++ server/src/graphql/Mutation/user/updateProfilePicture.ts
@@ -1,17 +1,14 @@
 import { localMessages, errorMessages, statusCodes } from "@constants";
 import {
-  deleteImage,
-  getImage,
   getUnauthorizedResponse,
   isLoggedIn,
   updateImage,
   uploadImage,
 } from "@utils";
-import { UserInputError } from "apollo-server-express";
+
 import { User } from "../../../schema/userSchema";
 
 const USER_PROFILE_PICTURES_FOLDER = process.env.CLOUDINARY_IMAGE_FOLDER || "";
-
 
 export const updateProfilePicture = async (
   _: undefined,
@@ -23,15 +20,24 @@ export const updateProfilePicture = async (
   }
   const userData = contextData.user;
   const userId = userData._id;
-
+  const errorResponse = {
+    response: {
+      message: errorMessages.USER.UPLOAD_IMAGE_FAILED,
+      status: statusCodes.BAD_REQUEST,
+    },
+  };
   try {
     if (!image) {
-      throw new UserInputError(errorMessages.USER.INVALID_USER_PROFILE_IMAGE);
+      return errorResponse;
     }
-    
-    const response =  userData?.profileImage?.publicId 
-    ? await updateImage(image, USER_PROFILE_PICTURES_FOLDER, userData.profileImage.publicId)
-    : await uploadImage(image, USER_PROFILE_PICTURES_FOLDER);
+
+    const response = userData?.profileImage?.publicId
+      ? await updateImage(
+          image,
+          USER_PROFILE_PICTURES_FOLDER,
+          userData.profileImage.publicId
+        )
+      : await uploadImage(image, USER_PROFILE_PICTURES_FOLDER);
 
     const { publicId, secureUrl } = response;
 
@@ -45,11 +51,17 @@ export const updateProfilePicture = async (
       },
       { new: true }
     );
-    return response;
-  } catch (error) {
     return {
-    message: errorMessages.USER.UPLOAD_IMAGE_FAILED,
-    status: statusCodes.BAD_REQUEST
-    }
+      profileImage: {
+        publicId,
+        secureUrl,
+      },
+      response: {
+        status: statusCodes.OK,
+        message: localMessages.USER.PROFILE_IMAGE_UPLOADED,
+      },
+    };
+  } catch (error) {
+    return errorResponse;
   }
 };
