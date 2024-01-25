ae45a72f1cb4e99784ab3cf41362cd1b7f0fafb7
Obliger0 <sujalgotharwal904@gmail.com>
2024-01-25 12:04:18 +0530

    PI-178: resolved review comments

--- server/src/graphql/Mutation/cities/insertCities.ts
+++ server/src/graphql/Mutation/cities/insertCities.ts
@@ -1,22 +1,27 @@
 import { cityModel } from "@models";
-import { statusCodes } from "@constants";
+import { errorMessages, localMessages, statusCodes } from "@constants";
 
 export const insertCities = async (
     parent: undefined,
     args: { citiesData: {cities : string[]} }
 ): Promise<CitiesOutputType> => {
+    const { INSERTION_FAILED } = errorMessages.CITIES;
     const errorData: CustomResponseType = {
-        message: "Failed",
+        message: INSERTION_FAILED,
         status: statusCodes.BAD_REQUEST,
     };
     try {
+        const {INSERTION_SUCCESS} = localMessages.CITIES;
         const { citiesData } = args;
         const cityData = await cityModel.create({
             cities:citiesData
         });
         return cityData ? {
             cityData: cityData.cities,
-            response: {message: "inserted", status: statusCodes.OK},
+            response: { 
+                message: INSERTION_SUCCESS,
+                status: statusCodes.OK
+            },
         } : {
             response : errorData,
         }
