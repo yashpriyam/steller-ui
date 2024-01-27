9ea01f9898fa660964d76bed82aef897375a140d
RitikShilpkar <ritikshilpkar23@gmail.com>
2024-01-27 11:57:03 +0530

    PI: send cookie to request from local storage

--- client/src/graphql/apolloClient/apolloClient.ts
+++ client/src/graphql/apolloClient/apolloClient.ts
@@ -1,11 +1,23 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = new HttpLink({
     uri: process.env.REACT_APP_SERVER_BASE_URL,
     credentials: 'include',
 })
 
+const cookie = localStorage.getItem(process.env.REACT_APP_JWT_SECRET_KEY || "")
+
+const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        cookie,
+      },
+    };
+  });
+
 export const apolloClient = new ApolloClient({
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
 })
