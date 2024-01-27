3b56b91e50451e1f932724d5dffa75da14e9b2b8
Obliger0 <sujalgotharwal904@gmail.com>
2024-01-27 16:48:09 +0530

    PI-179: UploadImage component added

--- client/src/components/uploadImage/uploadImage.tsx
+++ client/src/components/uploadImage/uploadImage.tsx
@@ -1,5 +1,6 @@
-import React, { MouseEventHandler, CSSProperties, ReactNode, FC, MouseEvent, ChangeEventHandler } from "react";
+import React, { CSSProperties, ReactNode, FC, ChangeEventHandler, useState, useEffect } from "react";
 import "./uploadImage.scss";
+import { CameraIcon } from "../../icons/index";
 interface UploadImageProps {
   className?: string;
   disable?: boolean;
@@ -7,6 +8,7 @@ interface UploadImageProps {
   text?: string | ReactNode;
   style?: CSSProperties;
   onChange?: ChangeEventHandler<HTMLInputElement>;
+  url?: string;
 }
 export const UploadImage: FC<UploadImageProps> = ({
   className,
@@ -15,22 +17,43 @@ export const UploadImage: FC<UploadImageProps> = ({
   text = "Upload Image",
   style,
   onChange,
+  url,
 }: UploadImageProps) => {
+  const [showPreview, setShowPreview] = useState<boolean>(false);
+  const handleClick = ()=>{
+    if(url) setShowPreview(true);
+    else setShowPreview(false);
+  }
+  useEffect(()=>{
+    handleClick();
+  },[url])
   return (
-    <div
-      className={`upload-image-main-container ${className} ${disable && "upload-image-disabled"}`}
-      style={style}
-    >
-      <input
-        type="file"
-        multiple={multiple}
-        id="file-type"
-        style={{ display: "none" }}
-        onChange={onChange}
-      />
-      <label className="upload-image-label" htmlFor="file-type">
-        <span>{text}</span>
-      </label>
+    <div className="upload-image-main-container">
+      <div
+        className={`upload-image-container 
+          ${className} 
+          ${ disable && "upload-image-disabled" }
+        `}
+        style={style}
+      >
+        <input
+          type="file"
+          multiple={multiple}
+          id="file-type"
+          style={{ display: "none" }}
+          onChange={onChange}
+          accept="image/*"
+        />
+        <label className={`upload-image-label ${showPreview && "upload-image-label-hide"}`} htmlFor="file-type">
+          <CameraIcon width="50px" height="50px"/>
+          <span>{text}</span>
+        </label> 
+        <img className={`upload-image-preview ${showPreview && "upload-image-preview-show"}`} src={url} alt="upload-preview" />
+      </div>
+      {/* {showPreview && (
+        <div className="image-preview-container">
+        </div>
+      )} */}
     </div>
   );
 };
