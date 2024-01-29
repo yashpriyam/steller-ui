d40ebb94c2fea88b1734a8fd26fe2ebce41d2935
Obliger0 <sujalgotharwal904@gmail.com>
2024-01-29 12:38:25 +0530

    PI-180: initial commit

---
+++ client/src/pages/userResponseProfile/userResponseProfile.tsx
@@ -0,0 +1,8 @@
+interface UserResponseProfilePage {
+  className: string;
+}
+export const UserResponseProfilePage: React.FC<UserResponseProfilePage> = ({
+  className,
+}: UserResponseProfilePage) => {
+    return <div></div>
+}
