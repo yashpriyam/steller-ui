b28a60bdc31db9f30b3d2c7100fc34407d24bc0a
Sachinrawat1215 <Sachinrawat0853@gmail.com>
2024-02-01 18:48:18 +0530

    change input type casing

--- client/src/graphql/query/userCode/getUserCode.ts
+++ client/src/graphql/query/userCode/getUserCode.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const GET_USER_CODE = gql`
-  query GetUserCode($input: getUserCodeInputType) {
+  query GetUserCode($input: GetUserCodeInputType) {
     getUserCode(input: $input) {
       data {
         questionId
