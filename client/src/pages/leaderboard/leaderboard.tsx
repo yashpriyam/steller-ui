ee9fd1340a5da10e095c28adfc7b745e4af4dbaa
RitikShilpkar <ritikshilpkar23@gmail.com>
2024-02-15 11:01:13 +0530

    change the leaderboard layout and color

--- client/src/pages/leaderboard/leaderboard.tsx
+++ client/src/pages/leaderboard/leaderboard.tsx
@@ -8,77 +8,55 @@ import { Star } from "../../icons/star";
 const LeaderBoard = () => {
   const { data: leaderboardData, loading } = useQuery(GET_LEADERBOARD_DATA);
 
-
-   // Filter the data to get only the user with rank 1
-   const firstPlaceUser = leaderboardData?.getLeaderBoardData?.find(
+  // Filter the data to get only the user with rank 1
+  const firstPlaceUser = leaderboardData?.getLeaderBoardData?.find(
     (data: any, idx: number) => idx === 0
   );
+
   return (
     <div className="leaderboardContainer">
-      <h1 className="leaderboardTitle">Leaderboard</h1>
       {loading ? (
         <Spinner />
       ) : (
         <div className="leaderboard">
-            {/* will use this section later */}
-        {/* <div className="firstPlaceUser">
-            <h2 className="firstPlaceTitle">First Place</h2>
-            <div className="userProfile">
-              {firstPlaceUser?.user?.profileImage ? (
-                <img
-                  src={firstPlaceUser.user.profileImage.secureUrl}
-                  alt={firstPlaceUser.user.name}
-                  className="profileImage"
-                />
-              ) : (
-                <div className="profileImage"></div>
-              )}
-              <span className="userName">{firstPlaceUser?.user.name}</span>
-            </div>
-            <p className="total-submissions">Total Submissions: {firstPlaceUser?.totalSubmissions}</p>
-          </div> */}
-      
-        <table className="leaderboardTable">
-          <thead>
-            <tr>
-              <th>Rank</th>
-              <th>User</th>
-              <th>Total Submissions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {leaderboardData?.getLeaderBoardData?.map(
-              (data: any, idx: number) => (
-                <tr key={data._id}>
-                  <td>
-                    {idx + 1}
-                    {
-                    idx === 0 && (
+          <table className="leaderboardTable ">
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>User</th>
+                <th>Total Submissions</th>
+              </tr>
+            </thead>
+            <tbody className="table-container">
+              {leaderboardData?.getLeaderBoardData?.map(
+                (data: any, idx: number) => (
+                  <tr key={data._id}>
+                    <td>
+                      {idx + 1}
+                      {idx === 0 && (
                         <div className="star">
-                            <Star/>
+                          <Star />
                         </div>
-                    )
-                  }
-                  </td>
-                 
-                  <div className="userProfile">
-                    {data?.user?.profileImage ? (
-                      <img
-                        src={data?.user.profileImage.secureUrl}
-                        alt={data?.user.name}
-                        className="profileImage"
-                      />
-                    ) : (
-                      <div className="profileImage"></div>
-                    )}
-                    <span className="userName">{data?.user.name}</span>
-                  </div>
-                  <td>{data.totalSubmissions}</td>
-                </tr>
-              )
-            )}
-          </tbody>
-        </table>
+                      )}
+                    </td>
+                    <td className="userProfile">
+                      {data?.user?.profileImage ? (
+                        <img
+                          src={data?.user.profileImage.secureUrl}
+                          alt={data?.user.name}
+                          className="profileImage"
+                        />
+                      ) : (
+                        <div className="profileImage"></div>
+                      )}
+                      <span className="userName">{data?.user.name}</span>
+                    </td>
+                    <td>{data.totalSubmissions}</td>
+                  </tr>
+                )
+              )}
+            </tbody>
+          </table>
         </div>
       )}
     </div>
