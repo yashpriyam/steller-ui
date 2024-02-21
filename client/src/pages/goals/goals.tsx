c1b9c342a8532a51e1b10e171a7af4bfc898eed5
RitikShilpkar <ritikshilpkar23@gmail.com>
2024-02-21 17:00:04 +0530

    refactor the code

--- client/src/pages/goals/goals.tsx
+++ client/src/pages/goals/goals.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useGoals } from "../../redux/actions/goalsAction"
 import Spinner from "../../components/spinner/spinner";
 import "./goals.scss";
@@ -7,35 +7,21 @@ import NoDataFoundComponent from "../../components/noDataFound/noDataFound";
 export const Goals = () => {
     const {getGoals,goals} = useGoals();
     const { goalsList, isGoalLoading } = goals || {};
-    const [showableDataOfGoals, setShowAbleDataOfGoals] = useState<Partial<GoalsType>[]>();
-    const goalDetailsToshow = [ "batchCode","title","description","frequency"];
-    const getShowableData = () => {
-        const showableData = goalsList?.map((data)=>({
-            title : data.title,
-            description : data.description,
-            frequency : data.frequency,
-            startWeek : data.startWeek,
-            endWeek : data.endWeek,
-        }))
-        return showableData;
-    }
+
     useEffect(() => {
         getGoals();
     },[])
-    useEffect(() => {
-        setShowAbleDataOfGoals(getShowableData());
-    },[goals])
+    
     return <div className="goals-page-main-container">
             <div className="goals-page-sub-container">
-        { isGoalLoading ? <Spinner/> : showableDataOfGoals ? showableDataOfGoals.map((data)=>(
+        { isGoalLoading ? <Spinner/> : goalsList ? goalsList.map((data)=>(
             <div className="goals-list-main-wrapper">
-                {
-                    Object.entries(data)?.map(([key, value],idx)=>(
-                         <div>
-                            {`${idx+1}). ${key} : ${value}`}
-                         </div>
-                    ))
-                }
+                <span>{data?.goalType?.goalType}</span>
+                <div>{data?.title}</div>
+                <div>{data?.description}</div>
+                <div>{data?.frequency}</div>
+                <div>{data.startWeek?.weekNumber}</div>
+                <div>{data?.endWeek?.weekNumber}</div>
                 <Button text="start" className="goals-start-button"/>
             </div>
             ))
