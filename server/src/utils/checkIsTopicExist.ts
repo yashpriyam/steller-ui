ebb602cfd0aa756bae78a96f17a4651eb2eb4dd8
pinkeshroy <pinkeshyadav9661@gmail.com>
2024-02-21 12:00:36 +0530

    fragmented checkistopicexist function

--- server/src/utils/checkIsTopicExist.ts
+++ server/src/utils/checkIsTopicExist.ts
@@ -1,27 +1,34 @@
 import { topicModel } from "@models";
+import { isSubTopicInSubTopicList } from "./isSubTopicInSubTopicList";
 export const checkIsTopicExist = async ({
   topic,
   subTopics,
 }: TopicSchemaType): Promise<boolean> => {
   try {
+    // Find the topic details in the database
     const topicDetails = await topicModel.findOne({ topic });
-    if(!topicDetails) return false;
+    // If no topic found, return false
+    if (!topicDetails) return false;
+    // Extract subtopic list from topic details
     const subTopicList = topicDetails?.subTopics;
+    // If the number of subtopics in the database is less than the provided subtopics, return false
     if (subTopicList && subTopicList.length < subTopics?.length) return false;
+    // If both provided and database subtopics exist, compare their titles
     if (subTopics?.length && subTopicList?.length) {
+      // Iterate over each provided subtopic
       for (let i = 0; i < subTopics.length; i++) {
-        let isTitleMatches = false;
-        for (let j = 0; j < subTopicList.length; j++) {
-          if (subTopics[i].title === subTopicList[j].title) {
-            isTitleMatches = true;
-            break;
-          }
-        }
+        const isTitleMatches = isSubTopicInSubTopicList({
+          subTopic: subTopics[i].title,
+          subTopicList: subTopicList
+        });
+        // If title match not found for any provided subtopic, return false
         if (!isTitleMatches) return false;
       }
     }
+    // All checks passed, return true
     return true;
   } catch (error) {
+    // Return false in case of any error
     return false;
   }
 };
---
+++ server/src/utils/isSubTopicInSubTopicList.ts
@@ -0,0 +1,13 @@
+export const isSubTopicInSubTopicList = ({
+  subTopic,
+  subTopicList,
+}: {subTopic:string,subTopicList:SubTopicType[]}) => {
+  for (let i = 0; i < subTopicList.length; i++) {
+    // Check if the title of the current provided subtopic matches with any subtopic in the database
+    if (subTopic === subTopicList[i].title) {
+      // If match found, set flag to true and exit loop
+      return true;
+    }
+  }
+  return false;
+};
