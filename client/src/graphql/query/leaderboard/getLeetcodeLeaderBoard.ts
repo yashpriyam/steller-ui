49a0f8f5e20cbee49166a3ef3f9118a800fd94aa
pinkeshroy <pinkeshyadav9661@gmail.com>
2024-02-29 17:43:58 +0530

    email and social link removed from leetcode api

--- client/src/graphql/query/leaderboard/getLeetcodeLeaderBoard.ts
+++ client/src/graphql/query/leaderboard/getLeetcodeLeaderBoard.ts
@@ -4,10 +4,8 @@ export const GET_LEETCODE_LEADERBOARD = gql`
   query Query {
     getLeetCodeLeaderboardData {
       users {
-        email
         name
         _id
-        socialLinks
         leetCodeUserProfile
         recentSubmissions {
           _id
