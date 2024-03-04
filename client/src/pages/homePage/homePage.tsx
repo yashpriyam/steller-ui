1e931484902cbaff3c534136cee66117d13dcca4
VIPL/17-18/CLL/025 <vipl17-18cll025@debs-MacBook-Pro.local>
2024-03-05 01:22:54 +0530

    tabs data test

--- client/src/pages/homePage/homePage.tsx
+++ client/src/pages/homePage/homePage.tsx
@@ -1,11 +1,35 @@
+import { Tabs } from '../../components/tabs/tabs';
 import React from 'react'
 
 /*TODO: @dhananjay - this page is WIP, adding here for example  */
 
-const HomePage = () => { 
+const HomePage = () => {
+  const dataList = [
+    {
+      text: " Tab 1",
+      value: "value1",
+      selected: true,
+    },
+    {
+      text: " Tab 2",
+      value: "value2",
+      disabled: true,
+    },
+    {
+      text: " Tab 3",
+      value: "value3",
+    },
+    {
+      text: " Tab 4",
+      value: "value4",
+    },
+  ];
+  console.log({ Map });
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <Tabs dataList={dataList} />
+    </div>
+  );
+};
 
 export default HomePage;
